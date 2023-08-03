697e811ae70ca3c1c7eaaed36885765c216e7567
dnljst <119126439+dnljst@users.noreply.github.com>
2023-08-03 19:15:26 +0200

    finished 1.4 (total excluded)

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,57 +1,27 @@
-const Header = (props) => {
-  console.log(props)
-
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
-
-const Part = (props) => {
-  console.log(props)
-
-  return (
-    <>
-      <p>{props.part} {props.exercise}</p>
-    </>
-  )
-}
-
-const Content = (props) => {
-  console.log(props)
- 
-  return (
-    <>
-      <Part part="Fundamentals of React" exercise= {10}/>
-      <Part part="Using props to pass data" exercise= {7}/>
-      <Part part="State of a component" exercise= {14}/>
-    </>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-
-  return (
-    <>
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>
-  )
-}
-
 const App = () => {
+  const course = "Half Stack application development"
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  }
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  }
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  }
 
   return (
     <div>
-      <Header course="Half Stack application development"/>
-      <Content />
-      <Total 
-      exercises1 = {10}
-      exercises2 = {7}
-      exercises3 = {14}/>
+      <h1>{course}</h1>
+      <p>{part1.name} {part1.exercises}</p>
+      <p>{part2.name} {part2.exercises}</p>
+      <p>{part3.name} {part3.exercises}</p>
     </div>
   )
 }
+
 
 export default App
