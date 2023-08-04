980d5d771af6d24ab91e4c23413fa3301e7dce19
dnljst <119126439+dnljst@users.noreply.github.com>
2023-08-04 13:02:12 +0200

    finished1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,27 +1,70 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <>
+    <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  
+  return (
+    <>
+    <p>{props.part.name} {props.part.exercises}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+
+  return (
+    <>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+
+  return (
+    <>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </>
+  )
+}
+
 const App = () => {
-  const course = "Half Stack application development"
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  }
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  }
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
   }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1.name} {part1.exercises}</p>
-      <p>{part2.name} {part2.exercises}</p>
-      <p>{part3.name} {part3.exercises}</p>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
-
 
 export default App
