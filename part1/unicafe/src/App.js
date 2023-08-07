8d90cfda55703bd18e0632b190184e6bddec8c4c
dnljst <119126439+dnljst@users.noreply.github.com>
2023-08-07 15:23:16 +0200

    finished 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const Statistics = (props) => {
+  return (
+    <>
+    <h1>statistics</h1>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+    </>
+  )
+}
+
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // handler
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text="good"></Button>
+      <Button handleClick={handleNeutral} text="neutral"></Button>
+      <Button handleClick={handleBad} text="bad"></Button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App;
